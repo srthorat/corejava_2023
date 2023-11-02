8cd9d30934da295db93f865f247074c41a904dd9
Sakharam Thorat <sakharam.thorat@greenwavesystems.com>
2023-11-02 16:48:38 +0530

    Day1 - commit2

---
+++ Day-1/DatatypesPremitive.java
@@ -0,0 +1,23 @@
+public class DatatypesPremitive {
+    public static void main(String a[]){
+        int num_1 = 3;              //integer 4 byte
+        System.out.println(num_1);
+        byte num_2 = 8;             // -128 to 127, 1 byte
+        System.out.println(num_2);
+        short num_3 = 155;          // 2 byte
+        System.out.println(num_3);
+        long num_4 = 155144413;     // 8 byte
+        System.out.println(num_4);
+        float num_5 = 12.6f;        // 4 byte
+        System.out.println(num_5);
+        double num_6 = 1233.88877;  // 8 byte
+        System.out.println(num_6);
+        char c = 'k';               // 2 byte, use singlequote
+        System.out.println(c);
+        boolean t = true;
+        System.out.println(t);
+
+
+    }
+    
+}
---
+++ Day-1/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String a[]){
+        int num_1 = 3; //integer
+        int num_2 = 5;
+        int result = num_1 + num_2;
+        System.out.println(result);
+
+    }
+    
+}
